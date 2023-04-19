5cfe9e2f978f5ea6e715e21808bc4166d8abcbb2
baedormann <89854178+baedormann@users.noreply.github.com>
2023-04-19 22:51:02 +0900

    [level 0] Title: 외계어 사전, Time: 3.71 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
@@ -0,0 +1,19 @@
+function solution(spell, dic) {
+    let cnt = 0;
+    
+    for(let word of dic){
+        let setWord = Array.from(new Set(word));
+        
+        console.log(setWord)
+        for(let char of spell){
+            if(setWord.includes(char)){
+                cnt++;
+            }
+        }
+        
+        if(cnt === spell.length) return 1;
+        cnt = 0;
+    }
+    
+    return 2;
+}

15c889813c871ced178a6d88f4f993eac4d3e306
baedormann <89854178+baedormann@users.noreply.github.com>
2023-05-08 21:41:03 +0900

    [unrated] Title: 배열 만들기 1, Time: 2.08 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.js
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.js
@@ -1,8 +1,7 @@
 function solution(n, k) {
     let arr = [];
     
-    for(let i = k; i <= n; i++)
-        if(i % k === 0) arr.push(i);
-    
-    return arr.sort((a, b) => a - b)
+    for(let i = k; i <= n; i++) if(i % k === 0) arr.push(i);
+        
+    return arr
 }

89f2f2a81ff8f350dbad6ebcaeafd4fa852d4ce1
baedormann <89854178+baedormann@users.noreply.github.com>
2023-05-09 21:33:27 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -0,0 +1,17 @@
+function solution(my_string) {
+    let sum = 0;
+    let sumStr = '';
+    
+    for(let i in my_string){
+        if(!Number.isNaN(parseInt(my_string[i])))
+            sumStr += my_string[i];
+        else{
+            sumStr === '' ? sum += 0 : sum += parseInt(sumStr);
+            sumStr = '';
+        }
+    }
+    
+    sumStr === '' ? sum += 0 : sum += parseInt(sumStr);
+    
+    return sum;
+}

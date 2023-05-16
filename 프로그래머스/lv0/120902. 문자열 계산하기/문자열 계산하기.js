81003b8199b530e5f57c63d6cf963d2b3ba71325
baedormann <89854178+baedormann@users.noreply.github.com>
2023-05-16 22:45:06 +0900

    [level 0] Title: 문자열 계산하기, Time: 2.83 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -0,0 +1,36 @@
+function solution(my_string) {
+    my_string = my_string.split('').filter(e => e !== ' ').map(e => Number.isNaN(parseInt(e)) ? e : parseInt(e) );
+    
+    let num = 0;
+    let total = 0;
+    let lastOper;
+    let nth = 1;
+    
+    for(let i = 0; i < my_string.length; i++){
+        let curItem = my_string[i];
+        if(curItem === '+' || curItem === '-'){
+            lastOper = curItem;
+            nth = 1;
+            total += num;
+            num = 0;
+            continue;
+        }
+        
+        
+        switch(lastOper){
+            case null:
+            case undefined: num = num * 10 + curItem;
+                break;
+            case '+': num = num * nth + curItem;
+                break;
+            case '-': num = num * nth - curItem;
+                break;
+        }
+        
+        nth = 10;
+        console.log(total)
+    }
+    total += num;
+    
+    return total;
+}
